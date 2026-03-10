0be2c76de5be3ac7d8813840b910aa281a3b314f
teyter <teg6@hi.is>
2026-03-10 14:28:13 +0000

    disable fokking autocomplete fokk

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,16 +1,6 @@
 return {
-  "saghen/blink.cmp",
-  opts = {
-    completion = {
-      -- Insert completion item on selection, don't select by default
-      list = {
-        selection = {
-          auto_insert = false,
-        },
-      },
-    },
-    keymap = {
-      preset = "super-tab",
-    },
+  {
+    "saghen/blink.cmp",
+    enabled = false,
   },
 }
